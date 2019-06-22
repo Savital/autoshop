a4b367cd89257bae3308b7f99b2eb38f3260fb3a
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-06-22 16:56:13 +0300

    Added simple tests

--- test_app.py
+++ test_app.py
@@ -4,10 +4,15 @@ import unittest
 
 
 class BasicTestCase(unittest.TestCase):
+    """initial test. ensure flask was set up correctly"""
     def test_index(self):
-        """initial test. ensure flask was set up correctly"""
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_favicon(self):
+        tester = app.test_client(self)
+        response = tester.get('/static/favicon.png', content_type="image/png")
         self.assertEqual(response.status_code, 200)
 
 
