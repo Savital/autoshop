8efc8df6a6276e362c88b7b53f7bad788311b32d
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-06-22 17:42:46 +0300

    Simple site structure

--- views.py
+++ views.py
@@ -4,5 +4,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def render_main(name=None):
-    return render_template('main.html', name=name)
+def render_index(name=None):
+    return render_template('index.html', name=name)
+
+
+@app.route('/about')
+def render_about(name=None):
+    return render_template('about.html', name=name)
