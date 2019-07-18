196268ac8f78251371b4a6049e07b017f8943827
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-07-18 11:41:50 +0300

    Simple site structure

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 # @Savital 22.06.19
 # config.py
 
+from app import app
+
 DEBUG = True
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[YOUR_DATABASE_NAME]'
+#db = SQLAlchemy(app)
