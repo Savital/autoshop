8e4b3d2c0295b9472c91856b7352e8d33de4beb5
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-07-18 12:18:01 +0300

    Linter doesn't work because of the Flask

--- autolint.bat
+++
@@ -1,5 +0,0 @@
-autopep8 --in-place --aggressive --aggressive models.py
-autopep8 --in-place --aggressive --aggressive views.py
-autopep8 --in-place --aggressive --aggressive config.py
-autopep8 --in-place --aggressive --aggressive app.py
-autopep8 --in-place --aggressive --aggressive test_app.py
